04c6c6717844e0e0264155f76c546c96e5191857
Varun Gulati <varungulati123@gmail.com>
2015-08-07 10:25:05 -0400

    Dronegama and Blog

--- dronegama/dronegama/urls.py
+++ dronegama/dronegama/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'dronegama.views.index', name='index'),
+    # url(r'^$', 'dronegama.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^testing/', include('testing.urls')),
